25e90090d03b428548084c03854b39df02677274
Tarun-Elango <tarunelango77@gmail.com>
2023-11-30 21:38:23 -0500

    css changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router';
 import { DownOutlined } from '@ant-design/icons';
 import { Dropdown, Space, Typography, Button, Input } from 'antd';
 import { useUser } from '@auth0/nextjs-auth0/client';
+import Head from 'next/head'
 
 export default function Home() {
    const {user, error, isLoading } = useUser();
@@ -124,9 +125,16 @@ export default function Home() {
   }, [currentAiIndex]);
   if (user){
   return (
-    
-    <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', backgroundColor: '#21262d', padding: '10px', overflowX:'hidden' }}>
-    <div style={{ display: 'flex', alignItems: 'center', width: '100%', maxWidth: '800px' }}>
+    <>
+    <Head>
+        <title>Assitant</title>
+        <meta name="description" content="Personal Assitant" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <link href="../../font/VarelaRound-Regular.ttf" rel="stylesheet"/>
+    </Head>
+    <div style={{  minHeight: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', backgroundColor: '#21262d', padding: '10px', overflowX:'hidden' }}>
+    <div style={{marginTop:'10px', display: 'flex', alignItems: 'center', width: '100%', maxWidth: '800px' }}>
         <h3 style={{ color: 'white', flex: '1', marginRight: '15px', fontSize: '1.2em' }}>{user.nickname} Welcome to Your AI Companion</h3>
         <Dropdown
             menu={{
@@ -142,12 +150,11 @@ export default function Home() {
             </Space>
             </Typography.Link>
         </Dropdown>
-        <Button onClick={handleBackButtonClick} style={{ backgroundColor: '#fa7970', marginLeft:'15px', border:'black' }}>Back</Button>
         
         <Button onClick={handleBackButtonClick} style={{ backgroundColor: '#fa7970', marginLeft:'15px', border:'black' }}>Log Out</Button>
     </div>
 
-    <div style={{ borderRadius: '10px', backgroundColor: '#89929b', marginTop: '10px', border: '1px solid black', padding: '10px', width: '100%', maxWidth: '800px', height: '70vh', overflowY: 'auto' }}>
+    <div style={{ borderRadius: '10px', backgroundColor: '#89929b', marginTop: '10px', border: '1px solid black', padding: '10px', width: '100%', maxWidth: '800px', height: '80vh', overflowY: 'auto' }}>
         {([...userMessages, ...responseMessages].sort((a, b) => new Date(a.timestamp) - new Date(b.timestamp))).map((message, index) => (
             <div key={index} style={{ textAlign: message.align, marginBottom: '10px' }}>
                 {message.text}
@@ -165,7 +172,7 @@ export default function Home() {
         <Button onClick={handleButtonClick} style={{ marginLeft: '10px', backgroundColor: '#7ce38b', border: 'black', fontSize: '1em' }} disabled={isButtonDisabled}>Send</Button>
     </div>
 </div>
-
+</>
   )
 
 } else if (isLoading) {
