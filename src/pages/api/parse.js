38ff61f812d892b04c80956e8f8275fa7c2091bf
Tarun-Elango <tarunelango77@gmail.com>
2024-02-04 15:26:46 -0500

    build fks

--- src/pages/api/parse.js
+++ src/pages/api/parse.js
@@ -93,7 +93,7 @@ apiRoute.post(async (req, res) => {
         const userRefID = userId
         const docuName = customNamespace
         console.log('here')
-        const response = await fetch('http://localhost:3000/api/document', { // Use a full URL if necessary
+        const response = await fetch('/api/document', { // Use a full URL if necessary
           method: 'POST',
           headers: {
               'Content-Type': 'application/json',
