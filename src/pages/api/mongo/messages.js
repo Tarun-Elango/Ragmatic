02fdcde9d119c0d56e7b7deef5bbcd5fa70e2673
Tarun-Elango <tarunelango77@gmail.com>
2024-04-26 17:44:47 -0400

    message logging

--- src/pages/api/mongo/messages.js
+++ src/pages/api/mongo/messages.js
@@ -23,18 +23,22 @@ export default async function handler(req, res) {
           const { chatID } = req.query;
           
           if (!chatID) {
+            console.log('all fields not present')
             return res.status(400).json({ success: false, message: 'Missing chatID' });
           }
+          console.log(`getting messages for chat: ${chatID}`)
     
           const messages = await Mes.find({ chatID });
           const userMessages = messages.filter(msg => msg.messageType === 'userMessage');
           const botMessages = messages.filter(msg => msg.messageType === 'botMessage');
     
+          console.log('found messages and returning')
           res.status(200).json({ 
             success: true, 
             data: { userMessages, botMessages } 
           });
         } catch (error) {
+          console.log('there was an error', error)
           res.status(500).json({ success: false, message: 'Server Error', error: error.message });
         }
       } 
@@ -44,13 +48,17 @@ export default async function handler(req, res) {
         try {
           const { chatID, userID, messageType, content } = req.body;
           if (!chatID || !userID || !messageType || !content) {
+            console.log('all fields not present')
             return res.status(400).json({ success: false, message: 'Missing required fields' });
           }
           
+          console.log(`creating new mesage for user ${userID} for chat ${chatID} and message type ${messageType}`)
           const newMessage = new Mes({ chatID, userID, messageType, content });
           const savedMessage = await newMessage.save();
+          console.log('message created')
           res.status(201).json({ success: true, data: savedMessage });
         } catch (error) {
+          console.log('there was an error')
           res.status(500).json({ success: false, message: 'Server Error', error: error.message });
         }
       } else {
