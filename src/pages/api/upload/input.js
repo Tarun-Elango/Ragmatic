6910dfed02ebe2a210a8bc6cdd59a074f8155c0c
Tarun-Elango <tarunelango77@gmail.com>
2024-04-26 21:45:55 -0400

    input logging

--- src/pages/api/upload/input.js
+++ src/pages/api/upload/input.js
@@ -1,19 +1,16 @@
 // parse api but for text files
 import { middleware } from "../../../middleware/middleware";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import axios from 'axios';
-
-const { createClient } = require('@supabase/supabase-js');
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 import OpenAI from 'openai';
-const openaio = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-  });
-// import { pipeline } from '@xenova/transformers'
-// const generateEmbedding = await pipeline('feature-extraction', 'Supabase/gte-small') 
 import connectDB from '../../../helper/mongodb';
 import Document from '../../../models/Document';
 connectDB();
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+const { createClient } = require('@supabase/supabase-js');
+const openaio = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+  });
+
 export default async function handler(req, res) {
   // const token =  req.headers.authorization;
   const result = await middleware(req);
@@ -23,56 +20,67 @@ export default async function handler(req, res) {
     res.status(400).json({ success: false, message: result.message });
   }
   if (req.method === 'POST') {
-    try{    // console.log('Request body:', req.body); // Log the entire body to debug
+    try{ 
+
+    console.log('starting input api route')
     const headerText = req.body.headerText;
     const bodyText = req.body.bodyText;
     const userid = req.body.userId
+
+    if (!headerText || !bodyText || !userid) {
+      console.log("all fields not present input api route")
+      return res.status(400).json({ error: 'headerText or bodyText or userid is not present in request', message:'headerText or bodyText or userid is not present in request' })
+    }
+    
     const customNamespace =`${userid}${headerText.replace("-", "").replace(/\s/g, "_")}`;;
+    console.log('name of the doc: ',customNamespace)
     let mongoResponse= null
 
-
+    /////////////////////////////////////////////////////////////////adding name to mongo
     try {
-    //add doc to mongodb directly
-    const existingUserDocu = await Document.findOne({  
-      userRefID: userid,
-      docuName: customNamespace
-    });
-    if (existingUserDocu) {
-        return res.status(200).json({ message:'File already exists' });
-    } else {
-        try {
-            const document = new Document({userRefID:userid, docuName:customNamespace });
-            await document.save();
-            mongoResponse = document
-           // return res.status(201).json({message:'Doc added success',result:document});
-        } catch (error) {
-            return res.status(500).json({ error: 'Error creating user\'s document' });
-        }
+      const existingUserDocu = await Document.findOne({  
+        userRefID: userid,
+        docuName: customNamespace
+      });
+      if (existingUserDocu) {
+          console.log('Document already exists in mongo db')
+          return res.status(200).json({ message:'File already exists' });
+      } 
+      const document = new Document({userRefID:userid, docuName:customNamespace });
+      await document.save();
+      console.log('document saved to mongo')
+      mongoResponse = document
+        // return res.status(201).json({message:'Doc added success',result:document});
+    } catch (error) {
+      console.log("mongo error: ", error)
+      return res.status(400).json({ error: 'MongoDb error', message:'MongoDb error' });
     }
-      } catch (error) {
-        return res.status(400).json({ error: 'MongoDb error', message:'MongoDb error' });
-      }
 
 
-
-    let textForSplit = ""
-    textForSplit = bodyText.replace(/\r?\n/g, ' ');
-    // The rest of the code remains the same since it operates on text.
-    const splitter = new RecursiveCharacterTextSplitter({
-        chunkSize: 500, // Adjust chunk size as needed
-        chunkOverlap: 40, // Adjust chunk overlap as needed
+      /////////////////////////////////////////////////////////////// splitting doc
+      let textForSplit = ""
+      textForSplit = bodyText.replace(/\r?\n/g, ' ');
+      // The rest of the code remains the same since it operates on text.
+      const chunk_size = 500
+      const chunk_overlap = 40
+      console.log(`splitting the doc chunk size ${chunk_size} and chunk overlap ${chunk_overlap}`)
+      const splitter = new RecursiveCharacterTextSplitter({
+          chunkSize: chunk_size, // Adjust chunk size as needed
+          chunkOverlap: chunk_overlap, // Adjust chunk overlap as needed
       });
       let test = await splitter.splitText(textForSplit)
       // const docOutput = await splitter.splitDocuments([
       //   new Document({ pageContent: textForSplit }),
       // ]);
       
-      console.log(test.length, 'length of doc');
+      console.log(`Document split in ${test.length} pages`);
 
-      // get the embeddings
+
+
+      /////////////////////////////////////////////////////////////// get the embeddings of pages
       let dataemb = null
       try {
-
+        console.log('getting embedding for all pages')
         const embeddingsResponse = await openaio.embeddings.create({
           input: test,
           model: 'text-embedding-3-small', // or another suitable engine,
@@ -82,7 +90,7 @@ export default async function handler(req, res) {
       
         // Parse the JSON response
         dataemb = embeddingsResponse;
-
+        console.log('got embedding for all pages')
       }catch(error){
         console.error('Failed to fetch embeddings:', error);
         return res.status(400).json({ error: 'Open ai embedding error', message:'Open ai embedding error' });
@@ -90,7 +98,10 @@ export default async function handler(req, res) {
 
 
     //console.log(dataemb.embeddings[0].embedding)
+    
 
+    /////////////////////////////////////////////////////////////// storing pages to supabase
+    console.log('creating array of pages, to be stored in supabas')
     const pageArray = []
     for (let i = 0; i < dataemb.data.length; i++) {
       const newPage = {
@@ -100,18 +111,23 @@ export default async function handler(req, res) {
         userid: userid ,                  // User ID
         content: test[i],               // Page Text
        };
-    pageArray.push(newPage);
+      pageArray.push(newPage);
     }
 
     const { data: pagesdata, error } = await supabase
         .from('pages')
         .insert(pageArray);
 
+    
     if (error) {
-        console.error('Error inserting pages:', error);
-    } else {
-        console.log('Inserted pages:', pagesdata);
-    }
+        console.error('Error inserting pages to supabase:', error);
+        // TODO: delete the document from mongo
+        return res.status(400).json({ error: 'Supabase error', message:'Error storing your file' });
+    } 
+
+    console.log('pages stored in supabase, of size', pageArray.length)
+    console.log('Inserted pages:', pagesdata);
+  
     res.status(200).json({
         message: 'File uploaded successfully',
       //   fileName: uploadedFile.originalname,
